9f480c451d95adf81b27069dfb0bc36bf12d36fa
Hero9868 <29997337+Hero9868@users.noreply.github.com>
2024-12-30 16:39:52 +0530

    updated scripts and removed devcontainer

---
+++ init_setup.sh
@@ -0,0 +1,24 @@
+echo [$(date)]: "START"
+
+
+echo [$(date)]: "creating env with python" 
+
+
+conda create --prefix ./env python -y
+
+# pixi init env
+# cd env
+# pixi add python
+
+
+echo [$(date)]: "activating the environment" 
+
+source activate ./env
+
+# pixi shell
+
+echo [$(date)]: "installing the dev requirements" 
+
+pip install -r requirements_dev.txt
+
+echo [$(date)]: "END" 
